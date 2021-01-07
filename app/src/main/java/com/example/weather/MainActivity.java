f51096237c365b56e76c5ed84d597763de3fe211
JakeKogut <jkogut@tfs.ca>
2021-01-07 09:27:14 -0500

    logcat works now

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -8,9 +8,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
     TextView mDate, mCity, mTemp, mDescription;
     ImageView imgIcon;
     String maVille="Toronto";
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        afficher();
         mDate=findViewById(R.id.mDate);
         mCity=findViewById(R.id.mCity);
         mTemp=findViewById(R.id.mTemp);
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void afficher(){
+        requestQueue= Volley.newRequestQueue(this);
         String url="http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=0c9bf96526cc8b6414068b4e6c2843ca&units=metric";
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -53,5 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        requestQueue.add(jsonObjectRequest);
     }
 }
