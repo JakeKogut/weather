a25cf3548ad9a97780078be2993c79906ad6f728
JakeKogut <jkogut@tfs.ca>
2021-01-07 08:59:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mDate, mCity, mTemp, mDescription;
+    ImageView imgIcon;
+    String maVille="Toronto";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDate=findViewById(R.id.mDate);
+        mCity=findViewById(R.id.mCity);
+        mTemp=findViewById(R.id.mTemp);
+        mDescription=findViewById(R.id.mDescription);
+
+    }
+
+    public void afficher(){
+        String url="http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=0c9bf96526cc8b6414068b4e6c2843ca&units=metric";
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject main_object=response.getJSONObject("main");
+                    JSONArray array = response.getJSONArray("weather");
+                    Log.d("Tag","resultat = "+array.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+}
