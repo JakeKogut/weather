944880f13ffd7eefd315a5aaeda10b894c4d9fd5
JakeKogut <jkogut@tfs.ca>
2021-01-15 10:01:09 -0500

    new icon

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -107,7 +107,8 @@ public class MainActivity extends AppCompatActivity {
                     String formatted_timeS=sdf.format(timeS);
 
                     mDate.setText(formatted_date);
-                    String imageUri="http://openweathermap.org/img/w/"+icon+".png";
+                    String imageUri="http://openweathermap.org/img/w/"+ icon+ ".png";
+                    imgIcon=findViewById(R.id.imgIcon);
                     Uri myUri= Uri.parse(imageUri);
                     Picasso.with(MainActivity.this).load(myUri).resize(200,200).into(imgIcon);
 
