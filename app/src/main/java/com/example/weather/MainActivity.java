d679be8f3090d343f5df67b6023c61b6d63340ff
JakeKogut <jkogut@tfs.ca>
2021-01-15 08:57:53 -0500

    new icon

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -13,16 +14,19 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     TextView mDate, mCity, mTemp, mDescription;
     ImageView imgIcon;
     String maVille="Toronto";
-    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +38,10 @@ public class MainActivity extends AppCompatActivity {
         mCity=findViewById(R.id.mCity);
         mTemp=findViewById(R.id.mTemp);
         mDescription=findViewById(R.id.mDescription);
-
+        afficher();
     }
 
     public void afficher(){
-        requestQueue= Volley.newRequestQueue(this);
         String url="http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=0c9bf96526cc8b6414068b4e6c2843ca&units=metric";
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -46,7 +49,29 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject main_object=response.getJSONObject("main");
                     JSONArray array = response.getJSONArray("weather");
-                    Log.d("Tag","resultat = "+array.toString());
+                 //   Log.d("Tag","resultat = "+array.toString());
+                    JSONObject object = array.getJSONObject(0);
+
+                    int tempC=(int)Math.round(main_object.getDouble("temp"));
+                    String temp=String.valueOf(tempC);
+
+                    String description=object.getString("description");
+                    String city=response.getString("name");
+                    String icon=object.getString("icon");
+
+                    mCity.setText(city);
+                    mTemp.setText(temp);
+                    mDescription.setText(description);
+
+                    Calendar calendar= Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, MMMM, dd");
+                    String formatted_date=simpleDateFormat.format(calendar.getTime());
+
+                    mDate.setText(formatted_date);
+                    String imageUri="http://openweathermap.org/img/w/"+icon+".png";
+                    Uri myUri= Uri.parse(imageUri);
+                    Picasso.with(MainActivity.this).load(myUri).resize(200,200).into(imgIcon);
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -58,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        requestQueue.add(jsonObjectRequest);
+        RequestQueue queue= Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
     }
 }
